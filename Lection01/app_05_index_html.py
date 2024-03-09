af957dc660010f3e350c8776600eb58b82d1d113
Ilia Mediankin <i.mediankin@gmail.com>
2024-03-09 22:06:26 +0300

    fix(lection01): minor changes

--- Lection01/app_05_index_html.py
+++ Lection01/app_05_index_html.py
@@ -10,8 +10,8 @@ def index():
 
 @app.route('/index/')
 def html_index():
-    # return render_template('index.html')
-    # Вернет ошибку, т.к. flask не видит 'index.html' в корне проекта
+    # return render_template('auth.html')
+    # Вернет ошибку, т.к. flask не видит 'auth.html' в корне проекта
     return render_template('index1.html')
     # index1.html необходимо разместить в '/static/templates'
 
